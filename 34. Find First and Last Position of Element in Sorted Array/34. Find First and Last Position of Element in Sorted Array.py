ce37f748c8c7033ccb672f9620add6762c2a34d4
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-01-26 15:36:24 +0200

    Update solution: 34. Find First and Last Position of Element in Sorted Array

--- 34. Find First and Last Position of Element in Sorted Array/34. Find First and Last Position of Element in Sorted Array.py
+++ 34. Find First and Last Position of Element in Sorted Array/34. Find First and Last Position of Element in Sorted Array.py
@@ -9,6 +9,7 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
+
         #Sol 5: O(logn) time & O(logn) space
 
         def helper(left,right,flag):
@@ -43,87 +44,87 @@ class Solution:
 
 
         
-        # #Sol 4: O(logn) time & O(1) space
+        #Sol 4: O(logn) time & O(1) space
 
-        # def findLeft(nums,target):
-        #     left,right,ans=0,len(nums)-1,-1
+        def findLeft(nums,target):
+            left,right,ans=0,len(nums)-1,-1
 
-        #     while left<=right:
-        #         mid=left+(right-left)//2
+            while left<=right:
+                mid=left+(right-left)//2
 
-        #         if nums[mid]==target:
-        #             ans=mid
-        #             right=mid-1
+                if nums[mid]==target:
+                    ans=mid
+                    right=mid-1
                 
-        #         elif nums[mid]<target:
-        #             left=mid+1
+                elif nums[mid]<target:
+                    left=mid+1
                 
-        #         else:
-        #             right=mid-1
+                else:
+                    right=mid-1
             
-        #     return ans
+            return ans
 
 
-        # def findRight(nums,target):
-        #     left,right,ans=0,len(nums)-1,-1
+        def findRight(nums,target):
+            left,right,ans=0,len(nums)-1,-1
 
-        #     while left<=right:
-        #         mid=left+(right-left)//2
+            while left<=right:
+                mid=left+(right-left)//2
     
-        #         if nums[mid]==target:
-        #             ans=mid
-        #             left=mid+1
+                if nums[mid]==target:
+                    ans=mid
+                    left=mid+1
 
-        #         elif nums[mid]<target:
-        #             left=mid+1
+                elif nums[mid]<target:
+                    left=mid+1
 
-        #         else:
-        #             right=mid-1
+                else:
+                    right=mid-1
 
-        #     return ans 
+            return ans 
 
-        # #find left and right edges separately
-        # left=findLeft(nums,target)
-        # right=findRight(nums,target)
+        #find left and right edges separately
+        left=findLeft(nums,target)
+        right=findRight(nums,target)
         
-        # return [left,right]
+        return [left,right]
 
 
 
         
-        # #Sol 3: brute force -> O(n) time & O(1) space -> binary search then traversal & new array of two positions only
+        #Sol 3: brute force -> O(n) time & O(1) space -> binary search then traversal & new array of two positions only
 
-        # left,right=0,len(nums)-1
-        # while left<=right:
-        #     mid=left+(right-left)//2
+        left,right=0,len(nums)-1
+        while left<=right:
+            mid=left+(right-left)//2
 
-        #     #instead of return -> iterate with two pointers again to get the edges
-        #     if nums[mid]==target:
-        #         left,right=mid,mid
+            #instead of return -> iterate with two pointers again to get the edges
+            if nums[mid]==target:
+                left,right=mid,mid
                 
-        #         while left-1>=0:
-        #             if nums[left-1]==target:
-        #                 left-=1
-        #             else:
-        #                 break
-        #         while right+1<len(nums):
-        #             if nums[right+1]==target:
-        #                 right+=1
-        #             else:
-        #                 break
+                while left-1>=0:
+                    if nums[left-1]==target:
+                        left-=1
+                    else:
+                        break
+                while right+1<len(nums):
+                    if nums[right+1]==target:
+                        right+=1
+                    else:
+                        break
                 
-        #         return [left,right]
+                return [left,right]
 
 
             
-        #     elif nums[mid]<target:
-        #         left=mid+1
+            elif nums[mid]<target:
+                left=mid+1
             
-        #     elif nums[mid]>target:
-        #         right=mid-1
+            elif nums[mid]>target:
+                right=mid-1
             
             
-        # return [-1,-1]
+        return [-1,-1]
 
 
 
@@ -136,16 +137,16 @@ class Solution:
 
 
 
-        # #Sol 1: brute force -> O(n) time & O(1) space
+        #Sol 1: brute force -> O(n) time & O(1) space
 
-        # ans=[-1,-1]
+        ans=[-1,-1]
 
-        # for i in range(len(nums)):
-        #     if nums[i]==target:
-        #         ans[1]=i
+        for i in range(len(nums)):
+            if nums[i]==target:
+                ans[1]=i
 
-        # for i in range(len(nums)-1,-1,-1):
-        #     if nums[i]==target:
-        #         ans[0]=i
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]==target:
+                ans[0]=i
         
-        # return ans
+        return ans
