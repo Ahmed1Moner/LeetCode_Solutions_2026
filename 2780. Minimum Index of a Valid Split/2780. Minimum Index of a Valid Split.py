3a06d3056bb47188e6f76c341e3711b6284095cc
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-01-27 06:24:55 +0200

    Update solution: 2780. Minimum Index of a Valid Split

--- 2780. Minimum Index of a Valid Split/2780. Minimum Index of a Valid Split.py
+++ 2780. Minimum Index of a Valid Split/2780. Minimum Index of a Valid Split.py
@@ -1,10 +1,14 @@
 #
 # Problem: 2780. Minimum Index of a Valid Split
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/minimum-index-of-a-valid-split/?envType=problem-list-v2&envId=w95808hv
+# Link: https://leetcode.com/problems/minimum-index-of-a-valid-split/description/?envType=problem-list-v2&envId=w95808hv
 # Language: python3
 # Date: 2026-01-27
 
+
+#try more optimal solution
+
+#formulated in first GOOO :)
 
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
@@ -22,7 +26,6 @@ class Solution:
             leftFreqSize=i+1
             rightFreqSize=len(indices)-leftFreqSize
 
-            #formulated from first time :)
             #dominance condition: dominant_count * 2 > subarray_size
             if (2*leftFreqSize>indices[i]+1) and (2*rightFreqSize>totalSize-indices[i]-1):
                 return indices[i]
