2e5da7d6117dd81b576ab30c6e37a01e8e834022
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-02-01 22:57:27 +0200

    Update solution: 3684. Maximize Sum of At Most K Distinct Elements

--- 3684. Maximize Sum of At Most K Distinct Elements/3684. Maximize Sum of At Most K Distinct Elements.py
+++ 3684. Maximize Sum of At Most K Distinct Elements/3684. Maximize Sum of At Most K Distinct Elements.py
@@ -1,7 +1,7 @@
 #
 # Problem: 3684. Maximize Sum of At Most K Distinct Elements
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/maximize-sum-of-at-most-k-distinct-elements/?envType=problem-list-v2&envId=waksgnnd
+# Link: https://leetcode.com/problems/maximize-sum-of-at-most-k-distinct-elements/description/?envType=problem-list-v2&envId=waksgnnd
 # Language: python3
 # Date: 2026-02-01
 
@@ -11,6 +11,24 @@
 class Solution:
     def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
 
-      #sol 1: O(nlogn) time & O(n) space
+        #sol 2: O(nlogn) time & O(K)~O(1) space
 
-      return sorted(set(nums), reverse=True)[:k]
+        nums.sort(reverse=True)
+
+        res=[nums[0]]
+        k-=1
+        i=1
+
+        while k>0 and i<len(nums):
+            if nums[i]!=nums[i-1]:
+                res.append(nums[i])
+                k-=1
+            i+=1
+
+        return res
+
+
+
+        #sol 1: O(nlogn) time & O(n) space
+
+        return sorted(set(nums), reverse=True)[:k]
