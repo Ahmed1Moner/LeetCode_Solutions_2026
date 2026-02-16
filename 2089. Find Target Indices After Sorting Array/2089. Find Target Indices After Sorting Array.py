066c11bf26d75d703a908e4d770e01647819223a
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-02-16 06:43:27 +0200

    Update solution: 2089. Find Target Indices After Sorting Array

--- 2089. Find Target Indices After Sorting Array/2089. Find Target Indices After Sorting Array.py
+++ 2089. Find Target Indices After Sorting Array/2089. Find Target Indices After Sorting Array.py
@@ -3,13 +3,46 @@
 # Difficulty: Easy
 # Link: https://leetcode.com/problems/find-target-indices-after-sorting-array/
 # Language: python3
-# Date: 2026-02-14
+# Date: 2026-02-16
 
 
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         
+        #sol 2: O(nlogn) time [nlogn sort + logn search + n iteration] & O(n) space
         
+        nums.sort()
+        left,right,found=0,len(nums)-1,-1
+
+        while left<=right:
+            mid=left+(right-left)//2
+
+            if nums[mid]==target:
+                found=mid
+                break
+
+            elif nums[mid]<target:
+                left=mid+1
+
+            else:
+                right=mid-1
+
+        if found==-1:
+            return []
+
+        ans=[]
+
+        left=found
+        while left>=0 and nums[left]==target:
+            ans.append(left)
+            left-=1
+
+        right=found+1
+        while right<len(nums) and nums[right]==target:
+            ans.append(right)
+            right+=1
+
+        return sorted(ans)
 
 
         #sol 1: O(nlogn) time & O(n) space
