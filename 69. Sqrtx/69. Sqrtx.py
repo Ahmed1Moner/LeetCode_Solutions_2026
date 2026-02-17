99813e069decdedcea1a4f19c2f02b796ddfde03
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-02-17 23:32:47 +0200

    Add solution: 69. Sqrt(x)

---
+++ 69. Sqrtx/69. Sqrtx.py
@@ -0,0 +1,28 @@
+#
+# Problem: 69. Sqrt(x)
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/sqrtx/description/
+# Language: python3
+# Date: 2026-02-17
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        #sol 1: O(logn) time & O(1) space
+
+        if x<2: return x
+
+        left,right=1,x//2
+        while left<=right:
+            mid=left+(right-left)//2
+
+            if mid*mid==x:
+                return mid
+
+            elif mid*mid<x:
+                left=mid+1
+            else:
+                right=mid-1
+
+        return right
