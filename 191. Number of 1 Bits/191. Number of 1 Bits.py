671e785e6d9b18de1d4dc5ace8ae43f87e138e9f
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-02-18 07:24:59 +0200

    Add solution: 191. Number of 1 Bits

---
+++ 191. Number of 1 Bits/191. Number of 1 Bits.py
@@ -0,0 +1,16 @@
+#
+# Problem: 191. Number of 1 Bits
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/number-of-1-bits/
+# Language: python3
+# Date: 2026-02-18
+
+
+#try divide and conquer
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        #sol 1: O(k) time & O(k) space -> k=logn & k:num of bits
+
+        return bin(n)[2:].count('1')
