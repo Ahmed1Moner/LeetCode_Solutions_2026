ba53032bf9fa0bdb900660b582f05b54e65d48af
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-02-23 23:45:32 +0200

    Update solution: 1. Two Sum

--- 1. Two Sum/1. Two Sum.py
+++ 1. Two Sum/1. Two Sum.py
@@ -1,7 +1,7 @@
 #
 # Problem: 1. Two Sum
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/two-sum/
+# Link: https://leetcode.com/problems/two-sum/description/
 # Language: python3
 # Date: 2026-02-23
 
@@ -9,7 +9,45 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        #sol 4: O(n) time & O(n) space
+
+        mapp={}
+        for i in range(len(nums)):
+            temp=target-nums[i]
+            
+            if temp in mapp:
+                return [i,mapp[temp]]
+            
+            mapp[nums[i]]=i
+
+
+        #sol 3: O(n^2) time & O(n) space
+
+        mapp=set(nums)
+
+        for i in range(len(nums)):
+            mapp.remove(nums[i])
+
+            temp=target-nums[i]
+            if temp in mapp:
+                return [nums.index(nums[i]),nums.index(temp)]
+            
+            mapp.add(nums[i])
+
+
+
+        #sol 2: O(n^2) time & O(1) space
+
+        for i in range(len(nums)):
+            temp=target-nums[i]
+            j=i+1
+
+            while j<len(nums):
+                if temp-nums[j]==0:
+                    return [i,j]
+                j+=1
         
+        return -1
 
 
         #sol 1: O(n^2) time & O(1) space
