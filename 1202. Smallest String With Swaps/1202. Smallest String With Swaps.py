6fddcd2196dbc65634fee38ea01016552958f266
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-03-07 15:40:55 +0200

    Update solution: 1202. Smallest String With Swaps

--- 1202. Smallest String With Swaps/1202. Smallest String With Swaps.py
+++ 1202. Smallest String With Swaps/1202. Smallest String With Swaps.py
@@ -1,17 +1,19 @@
 #
 # Problem: 1202. Smallest String With Swaps
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/smallest-string-with-swaps/description/
+# Link: https://leetcode.com/problems/smallest-string-with-swaps/
 # Language: python3
-# Date: 2026-03-06
+# Date: 2026-03-07
 
 
 #try bfs
 
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        
-        def dfs(node):
+    
+        #sol 1: O(n+m+nlogn) time & O(n+m) space
+
+        def dfs(node): #O(n+m) time & O(n) space
             seen.add(node)
             component.append(node)
             for neighbor in adjs[node]:
@@ -20,7 +22,7 @@ class Solution:
         
 
         adjs=defaultdict(list)
-        for u,v in pairs:
+        for u,v in pairs: #O(m) time & O(m) space
             adjs[u].append(v)
             adjs[v].append(u)
         
@@ -34,7 +36,7 @@ class Solution:
                 dfs(i)
 
                 chars=[s[i] for i in component]
-                chars.sort()
+                chars.sort() #O(nlogn) time
                 component.sort()
 
                 for i,ch in zip(component,chars):
