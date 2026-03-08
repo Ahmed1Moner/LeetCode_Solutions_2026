067a4b2f8ed470ce02a9988ea0bfaa6587850124
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-03-08 23:19:22 +0200

    Update solution: 1980. Find Unique Binary String

--- 1980. Find Unique Binary String/1980. Find Unique Binary String.py
+++ 1980. Find Unique Binary String/1980. Find Unique Binary String.py
@@ -1,13 +1,20 @@
 #
 # Problem: 1980. Find Unique Binary String
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/find-unique-binary-string/submissions/1942282774/?envType=daily-question&envId=2026-03-08
+# Link: https://leetcode.com/problems/find-unique-binary-string/?envType=daily-question&envId=2026-03-08
 # Language: python3
 # Date: 2026-03-08
 
 
+#try backtracking & recursion
+
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        
+        #sol 3: O(n) time & O(n) space
+
+        return ''.join('1' if nums[i][i]=='0' else '0' for i in range(len(nums))) #Cantor's diagonal argument
+
 
         #sol 2: O(n*(2**n)) time & O(n*(2**n)) space
 
