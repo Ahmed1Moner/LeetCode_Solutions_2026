5bf03b941caca9370c0ba0d5507891b421c5c4d6
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2026-03-09 12:09:08 +0200

    Update solution: 2656. Maximum Sum With Exactly K Elements

--- 2656. Maximum Sum With Exactly K Elements/2656. Maximum Sum With Exactly K Elements.py
+++ 2656. Maximum Sum With Exactly K Elements/2656. Maximum Sum With Exactly K Elements.py
@@ -1,13 +1,19 @@
 #
 # Problem: 2656. Maximum Sum With Exactly K Elements
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/?envType=problem-list-v2&envId=waksgnnd
+# Link: https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/description/?envType=problem-list-v2&envId=waksgnnd
 # Language: python3
 # Date: 2026-03-09
 
 
 class Solution:
     def maximizeSum(self, nums: List[int], k: int) -> int:
+
+        #sol 3: O(n) time & O(1) space
+
+        m=max(nums)
+        return m*k + (k*(k-1))//2
+
         
         #sol 2: O(n+k)~O(n) time & O(1) space
 
